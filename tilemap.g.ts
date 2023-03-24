5790c59ce9d12e3f7bf932f8c9a68132c2a2179d
hagencooperrr <123190644+hagencooperrr@users.noreply.github.com>
2023-03-24 08:25:00 -0700

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -5,10 +5,6 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
@@ -33,10 +29,6 @@ namespace myTiles {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
             case "tile1":return tile1;
-            case "myTile":
-            case "tile2":return tile2;
-            case "myTile0":
-            case "tile3":return tile3;
             case "tile4":return tile4;
         }
         return null;
